77f275557b7061cb3512e866bbecb5a55b0e5e99
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-08 14:19:05 -0500

    index js

---
+++ js/game.js
@@ -0,0 +1,19 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Brennan Lee All rights reserved
+//
+// Created by: Brennan Lee
+// Created on: nov 2022
+// This is the phaser3 game configuration file
+
+/**
+ * start phaser game.
+ */
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: 0x5f6e7a
+}
+const game = new Phaser.Game(config)
+console.log(game)
