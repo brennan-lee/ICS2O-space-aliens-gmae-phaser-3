5fdde427f26e83bd1d39e5545fcb86151574130e
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-10 14:22:28 -0500

    02 added

--- js/game.js
+++ js/game.js
@@ -9,11 +9,24 @@
 /**
  * start phaser game.
  */
-const config = {
+ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true
+   },
+ },
+   //set background color
   backgroundColor: 0x5f6e7a,
-};
+ scale: {
+     mode: phaser.scale.FIT,
+     //we replaced it in the middle of the page
+      autocenter: phaser.scale.CENTER_BOTH
+   },
+},
+
 const game = new Phaser.Game(config);
 console.log(game);
