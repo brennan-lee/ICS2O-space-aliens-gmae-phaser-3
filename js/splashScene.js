4a9601e4ff6aab7991ea87a0fea360af908221f5
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-11 14:39:54 -0500

    spaces in splash

--- js/splashScene.js
+++ js/splashScene.js
@@ -10,47 +10,47 @@
  * this class is the splash scene.
  */
 class SplashScene extends Phaser.Scene {
-  /**
+/**
    * This mathod is the construtor.
    */
-  constructor() {
+constructor() {
     super({ key: splahScene });
-  }
+}
 
-  /**
+/**
    * Can be defined on your own scenes
    * This mathod is called by the Scene Manager when  the scene start,
    *  before preload() and crate().
    *   @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  init(data) {
+init(data) {
     This.cameras.main.setBackgroundColor(ffffff);
-  }
-  /**
+}
+/**
    * can be defined on your own scenes.
    * use it to load assets.
    */
-  preload() {
+preload() {
     console.log("Splash Scene");
-  }
+}
 
-  /**
+/**
    * can be defined on your own scene.
    * use it to create your game objects.
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-  create(data) {
+create(data) {
     // pass
-  }
-  /**
+}
+/**
    * this should beoverridden by your own Scenes.
    * This mathod is called once per game step while the scene is running.
    * @param {number} time - The current time.
    * @param {number} delta - The delta time in ms since the last frame.
    */
-  update(time, delta) {
+update(time, delta) {
     // pass
-  }
+}
 }
 
 export default SplashScene;
