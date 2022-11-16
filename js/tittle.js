987bbbb1d79860f5434ce2efeee2628e691ffda6
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-16 14:16:18 -0500

    tittle .js

---
+++ js/tittle.js
@@ -0,0 +1,48 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Brennan Lee All rights reserved
+//
+// Created by: Brennan Lee
+// Created on: nov 2022
+// This is tittle Scene
+
+/**
+ *  This is the Tittle Scene.
+ */
+class TittleScene extends Phaser.Scnene {
+    /**
+     * This is the construtor.
+     */
+constructor(){
+    super({ key: "tittlescene"})
+}
+
+/**
+ * can be definded on your own scenes.
+ * this mathod is called by the scene manager when the scene starts,
+ * before payload() and crate().
+ * @param {object} data - any data passed via ScenePlugin.add() or ScenePlugin.start().
+ */
+init(data) {
+    this.cameras.main.setBackgroundcolor(ffffff)
+}
+
+/**
+ * this can be definded on your own scenes.
+ * use it to load assets.
+ */
+preload() {
+    console.log("Tittle Scene")
+}
+
+/**
+ * this can be definded on your own scenes.
+ * use it to crate your game objects.
+ * @param {number} time - current time
+ * @param {number} delta - the delta time in ms since last frame.
+ */
+update(time, delta) {
+    //pass
+}
+}
+export default TittleScene
