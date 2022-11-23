95e5e7cd9d3e6c4c88619b1a8685b2bcc1bac91f
brennan-lee <brennan-lee@users.noreply.github.com>
2022-11-23 21:51:56 +0000

    Prettified Code!

--- js/menuSence.js
+++ js/menuSence.js
@@ -7,52 +7,52 @@
 // This is Menu Scene
 
 /**
-* This class is the Menu Scene
-*/
+ * This class is the Menu Scene
+ */
 class MenuScene extends phaser.scene {
   /**
-  * This method is the construtor.
-  */
+   * This method is the construtor.
+   */
   constructor() {
-  super({ key: "menuScene"})
-}
+    super({ key: "menuScene" });
+  }
 
-/**
-  * Can be defined on your own scenes
+  /**
+   * Can be defined on your own scenes
    * This mathod is called by the Scene Manager when  the scene starts,
    *  before preload() and crate().
    *  @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-  */
-init(data) {
-  this.cameras.main.setBackgroundcolor("ffffff")
-}
+   */
+  init(data) {
+    this.cameras.main.setBackgroundcolor("ffffff");
+  }
 
-/**
+  /**
    * can be defined on your own scenes.
    * use it to load assets.
    */
-preload() {
-  console.log("Menu Scene")
-}
+  preload() {
+    console.log("Menu Scene");
+  }
 
   /**
    * can be defined on your own scene.
    * use it to create your game objects.
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
-crate(data) {
-  // pass
-}
+  crate(data) {
+    // pass
+  }
 
-/**
+  /**
    * this should beoverridden by your own Scenes.
    * This mathod is called once per game step while the scene is running.
    * @param {number} time - The current time.
    * @param {number} delta - The delta time in ms since the last frame.
    */
-update(time, delta) {
-  // pass
+  update(time, delta) {
+    // pass
   }
 }
 
-export default MenuScene
+export default MenuScene;
