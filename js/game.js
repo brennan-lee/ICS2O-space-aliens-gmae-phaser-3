7b151f3732ab16fad8e7e83c730980ec603363c3
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-23 19:49:26 +0000

    -6

--- js/game.js
+++ js/game.js
@@ -9,10 +9,12 @@
 // scene import statements
 import SplashScene from "./splashScene.js";
 import TitleScene from "./titleScene.js";
+import MenuScene from "./menuScene.js"
 
 // crate the new scenes
 const splashScene = new SplashScene();
 const titleScene = new TitleScene();
+const menuSecene = new MenuScene()
 
 /**
  * start phaser game.
@@ -43,6 +45,7 @@ console.log(game);
 // note: remember any "key" is global and CAN NOT be reused!
 game.scene.add("splashScene", splashScene);
 game.scene.add("titleScene", titleScene);
+game.scene.add("menuScene", menuScene)
 
 // the start scene
 game.scene.start("splashScene");
--- js/titleScene.js
+++ js/titleScene.js
@@ -15,6 +15,14 @@ class TitleScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "titleScene" });
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText - null
+    this.titleSceneTextStlye = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
   }
 
   /**
@@ -33,6 +41,7 @@ class TitleScene extends Phaser.Scene {
    */
   preload() {
     console.log("Tittle Scene");
+    this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
   }
 
   /**
@@ -41,7 +50,14 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.titleSceneBackgroundImage = this.add
+    .sprite(0, 0, "titleSceneBackground")
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2 
+
+    this.titleSceneText = this.add
+    .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+    .setOrigin(0.5)
   }
 
   /**
