99ae99e09ecd1cbac393c7f2565c92ec99f18e5c
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-12-09 19:01:38 +0000

    alien

--- js/gameScene.js
+++ js/gameScene.js
@@ -16,7 +16,7 @@ class GameScene extends Phaser.Scene {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920;
     let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50;
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
@@ -31,7 +31,7 @@ class GameScene extends Phaser.Scene {
  }
 
  init(data) {
-   this.cameras.main.setBackgroundColor("ffffff")
+   this.cameras.main.setBackgroundColor("#ffffff")
  }
 
  preload() {
@@ -41,7 +41,7 @@ class GameScene extends Phaser.Scene {
    this.load.image("starBackground", "./assets/starBackground.png")
    this.load.image("ship", "./assets/spaceShip.png")
    this.load.image("missile", "./assets/missile.png")
-   this.load.iamge("alien", "assets/alien.png")
+   this.load.image("alien", "./assets/alien.png")
    // sound
    this.load.audio("laser", "assets/laser1.wav")
  }
